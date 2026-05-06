73c3bcf431b2468c01420e0ac498db7df33af7a8
RAHUL-RAMA-DEV <rahulrd7@gmail.com>
2026-05-07 01:33:58 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,3 +6,24 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
 ]);
+  {
+    files: ["**/*.js"],
+    
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        test: "readonly"
+      }
+    },
+
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": "warn",
+      "no-undef": "error"
+    }
+  }
+];
