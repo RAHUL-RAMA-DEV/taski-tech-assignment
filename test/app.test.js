8a07170b17dfbf808597ba47dd96e5ed2d81891e
RAHUL-RAMA-DEV <rahulrd7@gmail.com>
2026-05-07 02:09:23 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,15 +1,15 @@
 const request = require('supertest');
-const app = require('../app');
- 
+const { app, server } = require('../app');
+
+ afterAll(() => {
+  server.close();
+  });
+
 describe('GET /',() => {
    it('should return Hello, Taski Technologies', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
         expect(res.text).toBe('Hello, Taski Technologies');
  });
-});
-const server = require("../app");
 
-afterAll(() => {
-  server.close();
-});
+
