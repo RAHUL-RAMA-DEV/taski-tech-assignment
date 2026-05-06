643cfb23af12d5e754d10d7412f137d33ff77f16
RAHUL-RAMA-DEV <rahulrd7@gmail.com>
2026-05-07 00:54:15 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ describe('GET /',() => {
    it('should return Hello, Taski Technologies', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
-        expect(res.text).toBe('Hello, Taski technologies');
+        expect(res.text).toBe('Hello, Taski Technologies');
  });
 });
