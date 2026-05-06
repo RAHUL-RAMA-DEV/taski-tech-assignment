12ab123aa7ca74adea621cc9b9ed454beb96fb04
RAHUL-RAMA-DEV <rahulrd7@gmail.com>
2026-05-07 03:21:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.get("/",(req,res) => {
    res.send("Hello, Taski Technologies");
 });
 
+const server = app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
+
 module.exports =  app; 
