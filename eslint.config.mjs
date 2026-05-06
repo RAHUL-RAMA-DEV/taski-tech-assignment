2df41e848ff34b806f39baf5569d7157b8cb06d4
RAHUL-RAMA-DEV <rahulrd7@gmail.com>
2026-05-07 01:36:07 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,10 +5,9 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
   {
     files: ["**/*.js"],
-    
+
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "commonjs",
@@ -26,4 +25,5 @@ export default defineConfig([
       "no-undef": "error"
     }
   }
-];
+]);
+ 
