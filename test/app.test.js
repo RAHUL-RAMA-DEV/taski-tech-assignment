b52269b369af8af933839c37b2fb402e4adbe30a
Rahul.R Dev <rahulrd7@gmail.com>
2026-05-07 08:47:54 +0000

    Edit app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,7 @@ const app = require("../app");
 describe("GET /", () => {
   test("should return hello message", async () => {
     const res = await request(app).get("/");
+
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe("Hello, Taski Technologies");
   });
