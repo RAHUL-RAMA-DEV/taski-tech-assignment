d66eb64ab4e4ef14d86f344fd33bfab6f241d6fc
Rahul.R Dev <rahulrd7@gmail.com>
2026-05-07 08:55:22 +0000

    Edit app.js

--- app.js
+++ app.js
@@ -7,9 +7,4 @@ app.get("/", (req, res) => {
   res.send("Hello, Taski Technologies");
 });
 
-// Start server
-app.listen(3000, () => {
-  console.log("server running on port 3000");
-});
-
 module.exports = app;
