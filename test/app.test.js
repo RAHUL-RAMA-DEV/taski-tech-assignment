7df1af6ac4fd7a889b7a644050a3e7210b6fdf3e
Rahul.R Dev <rahulrd7@gmail.com>
2026-05-07 08:00:10 +0000

    Edit app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,10 @@
-
 const request = require("supertest");
 const app = require("../app");
 
-describe("Integration Test - GET /", () => {
+describe("GET /", () => {
   test("should return hello message", async () => {
-    const response = await request(app).get("/");
-
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe("Hello, Taski Technologies");
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe("Hello, Taski Technologies");
   });
 });
